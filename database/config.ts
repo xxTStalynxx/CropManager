317926b88d2c4b746ef9d346d1c9c93cd4e497df
estalin <estalinmorocho18@gmail.com>
2024-03-27 18:00:55 -0500

    release_1.0

---
+++ database/config.ts
@@ -0,0 +1,4 @@
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_NAME = process.env.DB_NAME || 'dbmanager'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASS = process.env.DB_PASS || ''
--- database/connect.ts
+++ database/connect.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
+import { DB_HOST, DB_NAME, DB_PASS, DB_USER } from "./config";
 
-const db = new Sequelize('dbmanager','root','',{
-    host:'localhost',
-    dialect:'mysql'
-
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASS,{
+    host: DB_HOST,
+    dialect: 'mysql'
 })
 // const db = new Sequelize('dbmanager','postgres','admin',{
 //         host:'localhost',
